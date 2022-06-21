7dc2dc27b16e1136fa4a55d2280bea11505a6b3f
harujamu <tnagisa1220@gmail.com>
2022-06-21 13:25:03 +0000

    [Save]Bookers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,16 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def index
+    @books = Book.all
+  end
+  
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/#{book.id}' 
   end
 
   def show
