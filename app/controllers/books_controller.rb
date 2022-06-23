03d76d176a8723946790aa472ae4ffa2593fadf0
harujamu <tnagisa1220@gmail.com>
2022-06-23 13:50:11 +0000

    [Save](Create)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,23 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
 
+def new
+  @book = Book.new
+end
+
+
+# 直す
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    @books = Book.all
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render:index
+    end
   end
   
   def index
+    @book = Book.new
     @books = Book.all
   end
   
