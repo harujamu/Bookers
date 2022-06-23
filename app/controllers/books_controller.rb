067541e7e39460e941869a83bec2764b7cbf2baa
harujamu <tnagisa1220@gmail.com>
2022-06-23 05:37:26 +0000

    [Save](Create book)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,16 +3,16 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to book_path(book.id)
+  end
+  
   def index
     @books = Book.all
   end
   
-  def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/books/:id'
-  end
-
   def show
     @book = Book.find(params[:id])
   end
@@ -20,21 +20,25 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-  
-  def destroy
-    book = Book.find(params[:id])
-    book.destroy
-    redirect_to '/books'
-  end
-  
+   
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
   
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+ 
+  
   private
   def book_params
-    params.require(:book).permit(:title, :body)
+    params.permit(:title,:body)
+    # params.permit(:title,:body).require(:book)?
   end
+ 
 end
+
