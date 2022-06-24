1264a3129c726d71b4a6880ed03172e12133a910
harujamu <tnagisa1220@gmail.com>
2022-06-24 13:44:34 +0000

    [Save]Bookers(Index画面調整中)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,18 +1,11 @@
 class BooksController < ApplicationController
-
-def new
-  @book = Book.new
-end
-
-
 # 直す
   def create
     @book = Book.new(book_params)
-    @books = Book.all
     if @book.save
       redirect_to book_path(@book.id)
     else
-      render:index
+      render :index
     end
   end
   
@@ -44,8 +37,7 @@ end
   
   private
   def book_params
-    params.permit(:title,:body)
-    # params.permit(:title,:body).require(:book)?
+    params.require(:book).permit(:title,:body)
   end
  
 end
